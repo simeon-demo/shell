6927c78c965228a96f9d294bbc8ec5b6b9c4d4ef
simeon-demo <159085080+simeon-demo@users.noreply.github.com>
2025-03-25 12:15:03 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,7 +44,7 @@ rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -r ./data/processed > ./data/inventory.txt
+ls -R ./data/processed > ./data/inventory.txt
 
 
 
